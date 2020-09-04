5166294bcd30b33a9b8b4bf5c367b9944c8cc6f3
daniel_my <godchocho@gmail.com>
2020-09-04 10:32:04 +0700

    fix 'Sept-1/soal-1.js'

--- Sept-1/soal-1.js
+++ Sept-1/soal-1.js
@@ -1,8 +1,8 @@
 // Find the Faktor Persekutuan terbesar
 function fpb(number1, number2) {
     var temp = 0;
-    let small = number1;
-    if (number1 > number2) { small = number2 }
+    let small = Math.min(number1, number2);
+    // if (number1 > number2) { small = number2 }
     for (var i = 1; i < small; i++) {
         if (number1 % i === 0 && number2 % i === 0) {
             temp = i;
